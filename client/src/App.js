4ba5499b72a469818a091b9c3ff5bba5954c6d93
Yash Sharma <ys909@snu.edu.in>
2022-04-16 19:29:11 +0530

    edit pagination jsx

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import './index.css'
 import Auth from './components/Auth/Auth';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import PostDetails from './components/PostDetails/PostDetails'
 
 const App = () => {
-
+    const user = JSON.parse(localStorage.getItem('profile'))
 
     return (
         <BrowserRouter>
             <Header></Header>
             <Routes>
-                <Route path='/' exact element={<Home />} />
-                <Route path='/auth' exact element={<Auth />} />
+                <Route path='/' exact element={<Navigate replace to="/posts"/>} />
+                <Route path='/posts' exact element={<Home />} />
+                <Route path='/posts/search' exact element={<Home />} />
+                <Route path='/posts/:id' exact element={<PostDetails />} />
+                <Route path='/auth' exact element={!user ? <Auth /> : <Navigate replace to="/posts"></Navigate>} />
             </Routes>
         </BrowserRouter>
     )
